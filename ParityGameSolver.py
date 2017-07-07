5226caf1881b63ddc25ba7e3e82f2c60f84b48c8
Valo13 <valo13@live.nl>
2017-07-07 13:29:31 +0200

    paritygames can now be reduced

--- ParityGameSolver.py
+++ ParityGameSolver.py
@@ -4,7 +4,6 @@
 import os
 from FormulaReader import *
 
-
 model = None
 printInfo = False
 
@@ -18,54 +17,54 @@ class ParityGameNode:
             self.successors = successors  # if owner is EVEN or ODD this is a list of nodes, else a dict of nodes:prob
         else:
             self.successors = []
+        self.reductionStarted = False
+        self.state = None
+        self.formula = ""
+
+    def __str__(self):
+        if self.owner == "NATURE":
+            return str(self.nid) + ":NATURE:" + str([{n.nid: p} for n, p in self.successors.items()]) + "\n"
+        else:
+            return str(self.nid) + ":" + self.owner + ":" + str(self.rank) + ":" + str([n.nid for n in self.successors]) \
+                   + ":(" + str(self.state) + ", " + self.formula + ")" + "\n"
 
 
 class ParityGame:
-    def __init__(self, initNode, pNodes, nNodes):
+    def __init__(self, initNode, nodes):
         self.initNode = initNode
-        self.playerNodes = pNodes
-        self.natureNodes = nNodes
-        # set node ids
-        nid = 0
-        for s in range(len(self.playerNodes)):
-            for f in self.playerNodes[s]:
-                self.playerNodes[s][f].nid = nid
-                nid += 1
-        for nNode in natureNodes:
-            nNode.nid = nid
-            nid += 1
+        self.nodes = nodes
+        print(self.nodes)
 
     def __str__(self):
         out = ""
-        for s in range(len(self.playerNodes)):
-            for f in self.playerNodes[s]:
-                node = self.playerNodes[s][f]
-                out += str(node.nid) + ":" + node.owner + ":" + str(node.rank) + ":" + str([n.nid for n in node.successors])\
-                       + ":(" + str(s) + ", " + f + ")" + (":INIT" if node is self.initNode else "") + "\n"
-        for nNode in self.natureNodes:
-            out += str(nNode.nid) + ":NATURE:" + str([{n.nid: p} for n, p in nNode.successors.items()]) + "\n"
+        for node in self.nodes:
+            if node.owner == "NATURE":
+                out += str(node.nid) + ":NATURE:" + str([{n.nid: p} for n, p in node.successors.items()]) + "\n"
+            else:
+                out += str(node.nid) + ":" + node.owner + ":" + str(node.rank) + ":" + str([n.nid for n in node.successors]) \
+                       + ":(" + str(node.state) + ", " + node.formula + ")" + (":INIT" if node is self.initNode else "") + "\n"
         return out
 
-    def toDot(self, formulaName):
+    def toDot(self, formulaName, suffix=""):
         out = "digraph parityGame {\n"
-        for s in range(len(self.playerNodes)):
-            for f in self.playerNodes[s]:
-                node = self.playerNodes[s][f]
+        for node in self.nodes:
+            if node.owner == "NATURE":
+                out += "n" + str(node.nid) + "[shape=ellipse]\n"
+            else:
                 out += "n" + str(node.nid) + "[shape=" + ("box" if node.owner == "ODD" else "diamond") \
-                    + ", label=\"" + str(node.rank) + "\\n" + str(s) + "\\n" + f + "\"" + (",style=bold" if node is self.initNode else "") + "]\n"
-        for node in self.natureNodes:
-            out += "n" + str(node.nid) + "[shape=ellipse]\n"
-        for s in range(len(self.playerNodes)):
-            for f in self.playerNodes[s]:
-                node = self.playerNodes[s][f]
+                       + ", label=\"" + str(node.rank) + "\\n" + str(node.state) + "\\n" + node.formula + "\"" \
+                       + (",style=bold" if node is self.initNode else "") + "]\n"
+        for node in self.nodes:
+            if node.owner == "NATURE":
+                for succ, p in node.successors.items():
+                    out += "n" + str(node.nid) + " -> n" + str(succ.nid) + "[label=" + str(p) + "]\n"
+            else:
                 for succ in node.successors:
                     out += "n" + str(node.nid) + " -> n" + str(succ.nid) + "\n"
-        for node in self.natureNodes:
-            for succ, p in node.successors.items():
-                out += "n" + str(node.nid) + " -> n" + str(succ.nid) + "[label=" + str(p) + "]\n"
         out += "}"
-        f = open(os.path.sep.join([os.path.split(model.file)[0],
-                        "pg_" + os.path.basename(os.path.splitext(model.file)[0]) + "_" + formulaName + ".dot"]), 'w')
+        f = open(os.path.sep.join([os.path.split(model.file)[0], "pg_" +
+                                   os.path.basename(os.path.splitext(model.file)[0])
+                                   + "_" + formulaName + suffix + ".dot"]), 'w')
         f.write(out)
         f.close()
 
@@ -81,7 +80,7 @@ def nextRank(rank, becomesEven):
     if (rank % 2 == 0) == becomesEven:
         return rank
     else:
-        return rank+1
+        return rank + 1
 
 
 def createParityGameNodes(formula, state, rank, isProbabilistic):
@@ -101,7 +100,8 @@ def createParityGameNodes(formula, state, rank, isProbabilistic):
                 nNode = ParityGameNode("NATURE", rank)
                 createParityGameNodes(lambdaFormula(0.0), state, rank, isProbabilistic)
                 createParityGameNodes(lambdaFormula(1.0), state, rank, isProbabilistic)
-                nNode.successors = {playerNodes[state][lambdaFormula(0.0)]: 1-val, playerNodes[state][lambdaFormula(1.0)]: val}
+                nNode.successors = {playerNodes[state][lambdaFormula(0.0)]: 1 - val,
+                                    playerNodes[state][lambdaFormula(1.0)]: val}
                 natureNodes += [nNode]
         elif formula.op.type == "VAR":
             fixf = fixpointFormulas[formula.op.var]
@@ -129,7 +129,7 @@ def createParityGameNodes(formula, state, rank, isProbabilistic):
                     for s in transition.enddist:
                         createParityGameNodes(subf, s, rank, isProbabilistic)
                         if isProbabilistic:
-                            nsuccessors.update({playerNodes[s][str(subf)]: transition.enddist[s]})
+                            nsuccessors[playerNodes[s][str(subf)]] = transition.enddist[s]
                         else:
                             successors += playerNodes[s][str(subf)]
                     if isProbabilistic:
@@ -150,7 +150,77 @@ def createParityGameNodes(formula, state, rank, isProbabilistic):
 
 def createParityGame(formula, isProbabilistic=False):
     createParityGameNodes(formula, model.initstate, 0, isProbabilistic)
-    return ParityGame(playerNodes[model.initstate][str(formula)], playerNodes, natureNodes)
+    allNodes = []
+    # set node ids
+    nid = 0
+    for s in range(len(playerNodes)):
+        for f in playerNodes[s]:
+            node = playerNodes[s][f]
+            node.state = s
+            node.formula = f
+            node.nid = nid
+            allNodes += [node]
+            nid += 1
+    for nNode in natureNodes:
+        nNode.nid = nid
+        allNodes += [nNode]
+        nid += 1
+    return ParityGame(playerNodes[model.initstate][str(formula)], allNodes)
+
+
+def reduceParityGameRec(node):
+    if node.reductionStarted:
+        return node
+    else:
+        node.reductionStarted = True
+    if node.owner == "NATURE":
+        newSuccessors = {}
+        for succ, p in node.successors.items():
+            newSucc = reduceParityGameRec(succ)
+            newSuccessors[newSucc] = p
+        node.successors = newSuccessors
+
+        # if it only has one successor, simply return the successor instead
+        if len(node.successors) == 1:
+            return node.successors.keys()[0]
+        else:
+            return node
+    else:
+        for i in range(len(node.successors)):
+            node.successors[i] = reduceParityGameRec(node.successors[i])
+
+        # now the actual reductions
+        # remove successors that will certainly never be chosen (odd prio loop when even and dual)
+        newSuccessors = []
+        for succ in node.successors:
+            if not (succ.owner != "NATURE" and len(succ.successors) == 1 and succ.successors[0] is succ
+                    and ((node.owner == "EVEN" and succ.rank % 2 == 1) or (node.owner == "ODD" and succ.rank % 2 == 0))):
+                newSuccessors += [succ]
+        if not newSuccessors:
+            newSuccessors += [node.successors[0]]
+
+        # if it only has one successor, return that successor instead
+        #   change rank to minimum of both if successor is not a livelock
+        if len(newSuccessors) == 1:
+            newNode = newSuccessors[0]
+            if not (newNode.owner != "NATURE" and len(newNode.successors) == 1 and newNode.successors[0] is newNode):
+                newNode.rank = min(node.rank, newNode.rank)
+            return newNode
+        else:
+            node.successors = newSuccessors
+            return node
+
+
+def reachabilitySet(node, reachedNodes):
+    reachedNodes += [node]
+    for succ in node.successors:
+        if succ not in reachedNodes:
+            reachedNodes = reachabilitySet(succ, reachedNodes)
+    return reachedNodes
+
+
+def reduceParityGame(parityGame):
+    return ParityGame(parityGame.initNode, reachabilitySet(reduceParityGameRec(parityGame.initNode), []))
 
 
 def initParityGameSolver(ts, formula, store, verbose, isProbabilistic):
@@ -170,4 +240,5 @@ def initParityGameSolver(ts, formula, store, verbose, isProbabilistic):
         parityGame = createParityGame(formula)
     if parityGame is not None:
         parityGame.toDot(formula.name)
+        reduceParityGame(parityGame).toDot(formula.name, "_red")
     return None
